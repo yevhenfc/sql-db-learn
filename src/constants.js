900f44ec186c3c788b8c1d79c9d97cf44bc768ac
yevhenfc <yevhenfc@gmail.com>
2022-10-23 19:58:39 +0300

    updated all

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const ROLE = Object.freeze({
+    USER: 'USER',
+    ADMIN: 'ADMIN',
+    MODERATOR: 'MODERATOR'
+});
+
+export const ACTIONS = Object.freeze({
+    CREATE: 'CREATE',
+    READ: 'READ',
+    UPDATE: 'UPDATE',
+    DELETE: 'DELETE'
+});
